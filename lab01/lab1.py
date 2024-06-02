7022f7593f1cad298d4da08696af90743d2aef50
Larissa Matos <larissamatos07.lm@gmail.com>
2024-06-01 21:36:08 -0300

    adição de comentarios no lab1

--- lab01/lab1.py
+++ lab01/lab1.py
@@ -1,52 +1,37 @@
+import cv2 
 import sys
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
 
-img = cv2.imread(filename)
-im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-im_dst = cv2.imread(filename,0)
+# Para carregar uma imagem
+img = cv2.imread(filename) # Carrega a imagem
+im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converte a imagem para escala de cinza
+im_dst = cv2.imread(filename, 0) # Carrega a imagem. O 0 indica que a imagem será carregada em escala de cinza
 
-print(im)
-print(im.shape)
+print(im) # Exibe a matriz da imagem
+print(im.shape) # Exibe o tamanho da imagem
 
-width = im.shape[1]
-height = im.shape[0]
+width = im.shape[1] # Largura da imagem
+height = im.shape[0] # Altura da imagem
 
-#ler todos os pixels da imagem
-for c in range(0, width-1):
-	for l in range(0, height-1):
-		px = im.item(l,c)
+# Ler todos os pixels da imagem
+for y in range(0, height - 1):
+    for x in range(0, width - 1):
+        px = im.item(y, x) # Retorna o valor do pixel na posição (x, y)
+        im_dst.itemset(y, x, 255 - px) # Deixa a imagem negativa por meio da inversão dos valores dos pixels
 
-		#negativo
-		im_dst.itemset(l,c,255-px)
+# Redimensionar a imagem
+new_width = int(im.shape[1] * 0.5) # Nova largura
+new_height = int(im.shape[0] * 0.5) # Nova altura
 
-#		if px > 100:
-#			im.itemset(l,c,255)
+dim = (new_width, new_height) # Dimensões da nova imagem
+im_resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA) # Redimensiona a imagem
 
+ret, im_thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY) # Limiariza a imagem
 
-#redimensiona a imagem
-new_width = int(im.shape[1] * .5)
-new_height = int(im.shape[0] * .5)
-dim = (new_width, new_height)
-im_resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
-
-
-#threshold
-ret,im_thresh = cv2.threshold(im,127,255,cv2.THRESH_BINARY)
-
-#mostra imagem com opencv
-cv2.imshow('threashold',im_thresh)
-cv2.imshow('resized',im_resized)
-cv2.imshow('imagem',im)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-
-
-
-
+# Para exibir a imagem
+cv2.imshow('Imagem', im) # Exibe a imagem
+cv2.imshow('Threshold', im_thresh) # Exibe a imagem limiarizada
+cv2.imshow('Negativa', im_dst) # Exibe a imagem neagtiva
+cv2.waitKey(0) # Espera o usuário pressionar alguma tecla
+cv2.destroyAllWindows() # Fecha todas as janelas abertas
